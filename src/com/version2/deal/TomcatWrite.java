603ed115a7cbce257be474fd0f1b2fa3edfc45a6
zanebono <380207345@qq.com>
2018-07-12 11:20:33 +0800

    Tomcat_Status V1.0 you can make it to jar every java web project can use it just for mysql

--- src/com/version2/deal/TomcatWrite.java
+++ src/com/version2/deal/TomcatWrite.java
@@ -6,12 +6,76 @@ import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.management.MalformedObjectNameException;
+
 import cn.zucc.edu.cn.dao.TomcatStatusDao;
+import cn.zucc.edu.model.Connecting;
 import cn.zucc.edu.model.Connector;
 import cn.zucc.edu.model.Memory;
 import cn.zucc.edu.model.Memory_Pool;
 
 public class TomcatWrite {
+	public synchronized void release() {
+		TomcatStatusDao ts = new TomcatStatusDao();
+		String ipAddress=null;
+		int port = -1;
+		TomcatBeans tb = new TomcatBeans();
+		try {
+			ipAddress = tb.getIpAddress();
+			port = Integer.parseInt(tb.getPort());
+		} catch (MalformedObjectNameException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NullPointerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Connecting conn = new Connecting();
+		conn.setIpAddress(ipAddress);
+		conn.setPort(port);
+		conn.setStatus("断开连接");
+		int status=ts.Judge_connecting(ipAddress, port);
+		if(status==-1) {//没有记录
+			System.out.println("未被记录的连接断开");
+		}else if(status==1) {//有记录
+			ts.update_connecting(conn);
+		}
+		ts.release();
+	}
+	public synchronized void connect() {
+		TomcatStatusDao ts = new TomcatStatusDao();
+		String ipAddress=null;
+		int port = -1;
+		TomcatBeans tb = new TomcatBeans();
+		try {
+			ipAddress = tb.getIpAddress();
+			port = Integer.parseInt(tb.getPort());
+		} catch (MalformedObjectNameException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NullPointerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Connecting conn = new Connecting();
+		conn.setIpAddress(ipAddress);
+		conn.setPort(port);
+		conn.setStatus("已连接");
+		int status=ts.Judge_connecting(ipAddress, port);
+		if(status==-1) {//没有记录
+			conn.setName(ipAddress);
+			ts.add_connecting(conn);
+		}else if(status==1) {//有记录
+			ts.update_connecting(conn);
+		}
+		ts.release();
+	}
 	
 	public synchronized void write() {
 		StringBuffer sb = new StringBuffer();
@@ -19,6 +83,20 @@ public class TomcatWrite {
 		TomcatStatusDao ts = new TomcatStatusDao();
 		String ipAddress=null;
 		int port = -1;
+		TomcatBeans tb = new TomcatBeans();
+		try {
+			ipAddress = tb.getIpAddress();
+			port = Integer.parseInt(tb.getPort());
+		} catch (MalformedObjectNameException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NullPointerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		try {
 			for(TomcatInformations info : TomcatInformations.buildTomcatInformationsList()){
 				connector.setName(info.getName());
@@ -33,8 +111,6 @@ public class TomcatWrite {
 				connector.setBytesSent(info.getBytesSent());
 				connector.setPort(info.getPort());
 				connector.setIpAddress(info.getIpAddress());
-				port=info.getPort();
-				ipAddress=info.getIpAddress();
 				try {
 					ts.add_connector(connector);
 				} catch (SQLException e) {
